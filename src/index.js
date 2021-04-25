d1fb05645843cdd19237250d349b23bfdca704f4
Xu Wang <834166254@qq.com>
2021-04-25 16:38:01 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function getUrlParams(url) {
+    if (typeof url !== 'string') {
+        throw new Error('Parameter `url` must be a string.');
+    }
+
+    if (url.indexOf('?') > 0) {
+        query = url.substr(url.indexOf('?') + 1)
+    }
+
+    if (!query) return {};
+
+    var params = {};
+    var items = query.split('&');
+    var len = items.length;
+
+    while (len-- > 0) {
+        var _name = items[len].split('=')[0];
+        var value = items[len].split('=')[1] || true;
+        params[_name] = value;
+    }
+
+    return params;
+}
+
+module.exports = getUrlParams;
+
